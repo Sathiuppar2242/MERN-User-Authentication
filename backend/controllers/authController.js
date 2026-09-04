bc02ceaff79e9d47d24a2ef5c7cedaed4f027560
Sathish R <sathistake19@gmail.com>
2026-09-04 14:26:06 +0530

    Secure user passwords with bcrypt hashing

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,13 +1,25 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 const registerUser = async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "User with this email already exists"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         const user = await User.create({
             name,
             email,
-            password
+            password: hashedPassword
         });
 
         res.status(201).json({
