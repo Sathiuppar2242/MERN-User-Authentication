25fd4736337e8f71ca1fe1c3f6fe5fae03f06392
Sathish R <sathistake19@gmail.com>
2026-09-04 14:15:38 +0530

    Connect backend to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,18 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
 const app = express();
 
+// Connect to MongoDB
+connectDB();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Test route
 app.get("/", (req, res) => {
     res.json({
         message: "User Authentication API is running"
